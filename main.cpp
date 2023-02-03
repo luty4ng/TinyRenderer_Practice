5f78e0c08a70f88899a7ee072213584e01d59c04
Luty <lutydata@gmail.com>
2023-02-03 12:21:25 +0800

    Timings: fifth and final attempt

--- main.cpp
+++ main.cpp
@@ -7,43 +7,35 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
-	
-	bool steep = false;
-	if (std::abs(x0 - x1) < std::abs(y0 - y1))
-	{
-		std::swap(x0, y0);
-		std::swap(x1, y1);
-		steep = true;
-	}
-	if (x0 > x1)
-	{
-		std::swap(x0, x1);
-		std::swap(y0, y1);
-	}
+	bool steep = false; 
+    if (std::abs(x0-x1)<std::abs(y0-y1)) { 
+        std::swap(x0, y0); 
+        std::swap(x1, y1); 
+        steep = true; 
+    } 
+    if (x0>x1) { 
+        std::swap(x0, x1); 
+        std::swap(y0, y1); 
+    } 
 
-	// 利用像素单位和斜率计算y随x的单位增长
-	int dx = x1 - x0;
-	int dy = y1 - y0;
-	float derror = std::abs(dy / float(dx));
-	float error = 0;
-	int y = y0;
-	for (int x = x0; x <= x1; x++)
-	{
-		if (steep)
-		{
-			image.set(y, x, color);
-		}
-		else
-		{
-			image.set(x, y, color);
-		}
-		error += derror;
-		if (error > .5)
-		{
-			y += (y1 > y0 ? 1 : -1);
-			error -= 1.;
-		}
-	}
+	// 避免浮点运算，误差measure从斜率换为dy
+    int dx = x1-x0; 
+    int dy = y1-y0; 
+    int derror2 = std::abs(dy)*2; 
+    int error2 = 0; 
+    int y = y0; 
+    for (int x=x0; x<=x1; x++) { 
+        if (steep) { 
+            image.set(y, x, color); 
+        } else { 
+            image.set(x, y, color); 
+        } 
+        error2 += derror2; 
+        if (error2 > dx) { 
+            y += (y1>y0?1:-1); 
+            error2 -= dx*2; 
+        } 
+    } 
 }
 
 int main(int argc, char **argv)
